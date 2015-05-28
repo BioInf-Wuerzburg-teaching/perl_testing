9210f437d76ba82c4cc2e7c996f67be9d9661be2
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-05-28 11:04:19 +0200

    Usage of done_testing

--- more.pl
+++ more.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl -w
 
-use Test::More tests => 4;
+use Test::More;# tests => 4;
 
 ok( 1 + 1 == 2 , 'Result of 1+1==2');
 ok( 1 - 1 == 0, 'Result of 1-1==0' );
 is( 1 + 1,  2 , 'Result of 1+1==2');
 is( 1 - 1,  0, 'Result of 1-1==0' );
+
+done_testing();
