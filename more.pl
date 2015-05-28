be5c9ed5249a83a695e9f134d55978304cce8e8f
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-05-28 11:02:40 +0200

    Testing using Test::More

---
+++ more.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl -w
+
+use Test::More tests => 4;
+
+ok( 1 + 1 == 2 , 'Result of 1+1==2');
+ok( 1 - 1 == 0, 'Result of 1-1==0' );
+is( 1 + 1,  2 , 'Result of 1+1==2');
+is( 1 - 1,  0, 'Result of 1-1==0' );
